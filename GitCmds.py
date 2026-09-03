14c8467b3c65b7435a5cf1eea481a02fc543f8a4
kartikcicd <kartik@ci-cd.org>
2026-09-03 22:42:28 +0530

    Basic Git Commands

---
+++ GitCmds.py
@@ -0,0 +1,8 @@
+# 1. git status
+# Shows the current status of my repo - staged, unstaged, untracked files, etc.
+
+# 2. git add <file-name>
+# Stages a file for commit. You can also use "git add ." to stage all files in the current directory.
+
+# 3. git commit -m "commit message"
+# Commits the staged changes to the local repository with a descriptive message.
